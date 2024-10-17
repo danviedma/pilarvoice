2dddf3736ec2c2bfddb2efa9f917fa3b0cdc82b8
Alejandro Egana <danviedma@gmail.com>
2024-10-17 17:34:58 -0300

    fixed social media icons added

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -2,55 +2,10 @@ document.getElementById('hamburger').addEventListener('click', function () {
     this.classList.toggle('active');
     document.getElementById('nav-links').classList.toggle('active');
     
-    // Agrega o quita la clase al main cuando el menú está activo
     const main = document.querySelector('main');
     main.classList.toggle('menu-open');
 });
 
-/*
-document.addEventListener('DOMContentLoaded', () => {
-    const container = document.getElementById('testimonials-container');
-    const testimoniosUrl = './assets/data/testimonios.json';
-    let testimonios = [];
-    let currentIndex = 0;
-    const testimoniosPorCarga = 1; // Cargar uno por uno
-
-    // Cargar el JSON
-    fetch(testimoniosUrl)
-        .then(response => response.json())
-        .then(data => {
-            testimonios = data.sort(() => Math.random() - 0.5); // Mezcla los testimonios aleatoriamente
-            cargarMasTestimonios(); // Cargar el primer testimonio
-        })
-        .catch(error => console.error('Error al cargar los testimonios:', error));
-
-    function crearTestimonioCard(testimonio, index) {
-        const card = document.createElement('div');
-        card.classList.add('card');
-        card.style.setProperty('--delay', `${index * 5}s`); // Retraso progresivo de 5 segundos por tarjeta
-        card.innerHTML = `
-            <h3>${testimonio.nombre}</h3>
-            <p>${testimonio.texto}</p>
-        `;
-        return card;
-    }
-
-    function cargarMasTestimonios() {
-        for (let i = 0; i < testimoniosPorCarga && currentIndex < testimonios.length; i++) {
-            const testimonio = testimonios[currentIndex];
-            container.appendChild(crearTestimonioCard(testimonio, i)); // Pasamos el índice para el retraso
-            currentIndex++;
-        }
-    }
-
-    // Evento para detectar el scroll y cargar más testimonios uno por uno
-    window.addEventListener('scroll', () => {
-        if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-            cargarMasTestimonios();
-        }
-    });
-});
-*/
 
 document.addEventListener('DOMContentLoaded', () => {
     const container = document.getElementById('testimonials-container');
